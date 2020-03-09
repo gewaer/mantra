60305a3b848bac7e7b3ab7881b172cc9acfd4479
Carlos Polanco <carlospolanco@mctekk.com>
2020-03-09 16:30:05 -0400

    Added a new Mantra Functional Component

---
+++ lib/components/MantraForm.vue
@@ -0,0 +1,26 @@
+<script>
+/*
+    MISSING IMPLEMENTATIONS:
+    1. Route utility
+    2. MantraForm class (static method "getConfig" usage)
+    3. Render a component using MantraForm configuration
+*/
+import MantraForm from './../core/components/MantraForm';
+const convertRouteToPath = function (route) {
+    const { params } = route;
+    const path = Object.values(params).join('.');
+    return path;
+};
+
+export default {
+    functional: true,
+    beforeRouteEnter(to, from, next) {
+        const path = convertRouteToPath(to);
+        const config = MantraForm._getConfig({ path });
+        next();
+    },
+    render(createElement, context) {
+        // Component is render using CreateElement
+    }
+};
+</script>
