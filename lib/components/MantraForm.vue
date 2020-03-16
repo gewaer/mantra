b01999408ec8c2e56606edd373d62d380ea5fa2f
Carlos Polanco <carlosfollows@gmail.com>
2020-03-16 16:11:45 -0400

    WIP - Integrating MantraData and MantraForm in functional component

--- lib/components/MantraForm.vue
+++ lib/components/MantraForm.vue
@@ -6,10 +6,17 @@
     3. Render a component using MantraForm configuration
 */
 import MantraForm from './../core/components/MantraForm';
+
 const convertRouteToPath = function (route) {
     const { params } = route;
     const path = Object.values(params).join('.');
     return path;
+};
+
+const pushToRouteParams = function (route, payload) {
+    for (const [key, value] of Object.entries(payload)) {
+        route.params[key] = value;
+    }
 };
 
 export default {
@@ -17,17 +24,21 @@ export default {
     beforeRouteEnter(to, from, next) {
         const path = convertRouteToPath(to);
         const config = MantraForm._getConfig({ path });
+        const IS_PATH_EMPTY = (config._path === '');
         
-        const IS_VALID_COMPONENT = config.component._isValidComponent(this);
-        const IS_PATH_EMPTY = (config.path === '');
-        
-        if (IS_VALID_COMPONENT && IS_PATH_EMPTY) {
+        if (IS_PATH_EMPTY) {
+            pushToRouteParams(to, { config });
+            console.log("[beforeRouteEnter] ROUTE OBJECT", to);
             next();
         } else {
             next('*');
         }
     },
     render(createElement, context) {
+        console.log("[render] CONTEXT", context);
+        const { props: { config }, parent } = context;
+        const IS_VALID_COMPONENT = config.component._isValidComponent(parent);
+        console.log("IS VALID COMPONENT", IS_VALID_COMPONENT);
         // Component is render using CreateElement
         createElement('GeneralVehicle', context);
     }
