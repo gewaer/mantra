2364fe4604dfcd06291840c48561f945751d827d
Carlos Polanco <carlosfollows@gmail.com>
2020-03-26 12:29:14 -0400

    Added support for testing vue components using `@vue/test-utils` library

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,17 @@
 const path = require('path');
+const {defaults} = require('jest-config');
 
 module.exports = {
 	rootDir: path.join(__dirname, './test'),
 	displayName: 'client',
-	testEnvironment: 'jest-environment-node',
+	testEnvironment: 'jest-environment-jsdom',
 	moduleDirectories: ['node_modules'],
+	moduleFileExtensions: [...defaults.moduleFileExtensions, 'vue'],
 	testMatch: ['**/test/unit/**/*.js'],
 	collectCoverageFrom: ['**/lib/**/*.js'],
-	watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname']
+	watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+	transform: {
+		'^.+\\.[t|j]sx?$': 'babel-jest',
+		'.*\\.(vue)$': 'vue-jest'
+	}
 };
