00b02b8af085cc33d5717e666a1a0f3bec3d0856
Carlos Polanco <carlosfollows@gmail.com>
2020-03-27 09:45:45 -0400

    Added the jest configuration for the `jest-extend` plugin

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,13 @@ const {defaults} = require('jest-config');
 
 module.exports = {
 	rootDir: path.join(__dirname, './test'),
-	displayName: 'client',
 	testEnvironment: 'jest-environment-jsdom',
 	moduleDirectories: ['node_modules'],
 	moduleFileExtensions: [...defaults.moduleFileExtensions, 'vue'],
 	testMatch: ['**/test/unit/**/*.js'],
 	collectCoverageFrom: ['**/lib/**/*.js'],
 	watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+	setupFilesAfterEnv: ['jest-extended'],
 	transform: {
 		'^.+\\.[t|j]sx?$': 'babel-jest',
 		'.*\\.(vue)$': 'vue-jest'
